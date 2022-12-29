efdecaee2ca5572038431114d5b224b4a88fba63
Sarah Allen <sarah@ultrasaurus.com>
2022-12-29 13:45:44 -0800

    address feedback from code review

--- examples/kanji_info.rs
+++ examples/kanji_info.rs
@@ -2,24 +2,21 @@
 * SPDX-License-Identifier: Apache-2.0 Refer to the file "LICENSE" for details.
 * example illustrating how to access kanji and reading information for a word
 *******************************************************************************/
-use jmdict::{KanjiElement, ReadingElement};
 
 fn main() {
     let input = "一日";
     let count = jmdict::entries()
         .filter(|e| {
-            let mut result = false;
             if e.kanji_elements().any(|k| k.text == input) {
-                result = true;
+                // note two entries have identical Kanji and reading
+                // yet differ in "sense" not shown in this example
                 println!("--- entry #{} ---", e.number);
-                let kanji_elements: Vec<KanjiElement> = e.kanji_elements().map(|ke| ke).collect();
-                for kanji in &kanji_elements {
+                for kanji in e.kanji_elements() {
                     println!("kanji element: {}", kanji.text);
                     println!("   priority: {:?}\n", kanji.priority);
                 }
 
-                let reading_forms: Vec<ReadingElement> = e.reading_elements().map(|item| item).collect();
-                for reading in reading_forms {
+                for reading in e.reading_elements() {
                     println!("reading_form: {}", reading.text);
                     println!("   priority: {:?}\n", reading.priority);
 
@@ -28,11 +25,9 @@ fn main() {
                     }
                 }
 
-
-                // let sense = e.senses().next().unwrap().text;
-
+                return true
             }
-            result
+            false
         })
         .count();
     println!("{} entries for {}", count, input);
