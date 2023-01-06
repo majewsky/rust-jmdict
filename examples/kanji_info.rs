e26e3b05cb0514687f37d6d9e10b0605d0924b5c
Stefan Majewsky <stefan.majewsky@sap.com>
2023-01-06 15:52:45 +0100

    kanji_info example: simplify using .inspect()

--- examples/kanji_info.rs
+++ examples/kanji_info.rs
@@ -6,28 +6,24 @@
 fn main() {
     let input = "一日";
     let count = jmdict::entries()
-        .filter(|e| {
-            if e.kanji_elements().any(|k| k.text == input) {
-                // note two entries have identical Kanji  
-                // yet differ in reading
-                println!("--- entry #{} ---", e.number);
-                for kanji in e.kanji_elements() {
-                    println!("kanji element: {}", kanji.text);
-                    println!("   priority: {:?}\n", kanji.priority);
-                }
-
-                for reading in e.reading_elements() {
-                    println!("reading_form: {}", reading.text);
-                    println!("   priority: {:?}\n", reading.priority);
-
-                    for info in reading.infos() {
-                        println!("info: {:?}", info);
-                    }
-                }
-
-                return true
+        .filter(|e| e.kanji_elements().any(|k| k.text == input))
+        .inspect(|e| {
+            // note two entries have identical Kanji
+            // yet differ in reading
+            println!("--- entry #{} ---", e.number);
+            for kanji in e.kanji_elements() {
+                println!("kanji element: {}", kanji.text);
+                println!("   priority: {:?}\n", kanji.priority);
             }
-            false
+
+            for reading in e.reading_elements() {
+                println!("reading_form: {}", reading.text);
+                println!("   priority: {:?}\n", reading.priority);
+
+                for info in reading.infos() {
+                    println!("info: {:?}", info);
+                }
+            }
         })
         .count();
     println!("{} entries for {}", count, input);
